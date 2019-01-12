1dbbf1c31be7a06e78481e350bdd7b0566afd3fc
Mirza Leka <mirza_376@hotmail.com>
2019-01-12 23:12:01 +0100

    Added like-dislike buttons

---
+++ LikeDislike/index.js
@@ -0,0 +1,21 @@
+var btn1 = document.querySelector('#green');
+var btn2 = document.querySelector('#red');
+
+btn1.addEventListener('click', function() {
+  
+    if (btn2.classList.contains('red')) {
+      btn2.classList.remove('red');
+    } 
+  this.classList.toggle('green');
+  
+});
+
+btn2.addEventListener('click', function() {
+  
+    if (btn1.classList.contains('green')) {
+      btn1.classList.remove('green');
+    } 
+  this.classList.toggle('red');
+  
+});
+
